86a64c342f4225cf8a76a0730f4fd89ef0ab449b
joemiller <joemiller4500@gmail.com>
2020-09-22 22:09:01 -0700

    New Plan

--- .travis/make.py
+++ .travis/make.py
@@ -12,7 +12,7 @@ from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
-ALPHA_KEY = os.environ('ALPHA_KEY')
+ALPHA_KEY = server.connect(os.environ['MONGO_URL'])
 ts = TimeSeries(key=ALPHA_KEY,output_format='pandas')
 
 skipTrain = True
