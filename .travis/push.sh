406905dfa844e4707268c016f211d6c7fa5a0c6b
joemiller <joemiller4500@gmail.com>
2020-09-25 10:28:22 -0700

    Back to push

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,5 +9,11 @@ commit_website_files() {
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
+upload_files() {
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/joemiller4500/stock-predictor.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
 setup_git
 commit_website_files
+upload_files
