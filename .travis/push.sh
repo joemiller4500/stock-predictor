449d67e912698aed6f4053cd5091950638522531
joemiller <joemiller4500@gmail.com>
2020-10-12 20:18:45 -0700

    switched to master from pages

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,13 +7,13 @@ commit_website_files() {
   git checkout master
   # git add . *.png
   # git add . *.csv
-  git add .
+  git add static
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
   # git push
 }
 
 upload_files() {
-  # git remote add origin-pages https://${GITHUB_TOKEN}@github.com/joemiller4500/stock-predictor.git > /dev/null 2>&1
+  git remote add https://${GITHUB_TOKEN}@github.com/joemiller4500/stock-predictor.git > /dev/null 2>&1
   git push --quiet origin master 
 }
 
